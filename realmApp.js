80e766b9ab4df96c93e6a26946ea1a6c3cec3886
rajarshi1 <ghoshal.rajarshi@gmail.com>
2022-09-06 18:51:03 +0530

    first commit

--- realmApp.js
+++ realmApp.js
@@ -2,6 +2,6 @@ import Realm from "realm";
 
 
 // Invokes the shared instance of the Realm app.
-const app = new Realm.App({id: "<your Realm app ID here>"}); // Set Realm app ID here.
+const app = new Realm.App({id: "tasktracker-dbghz"}); // Set Realm app ID here.
 export default app;
 
